1150f6cdbc4aa820c101014a3183e5346a1a9145
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2023-11-27 22:04:47 -0300

    fix table name

--- api/init.sql
+++ api/init.sql
@@ -8,7 +8,7 @@ create table artist (
     name text
 );
 
-create table artist_genres (
+create table artist_genre (
     artist_id bigint references artist (id),
     genre_id bigint references genre (id),
     primary key (artist_id, genre_id)
